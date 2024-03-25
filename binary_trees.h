2aa8615193d8ca89682967cb97e8db606b9e7c29
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-03-25 17:06:11 +0000

    Add four mandatory tasks

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,13 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
